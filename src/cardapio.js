609fcd34ead4ca458233408476f817eeaf16513a
Emerson Torres <36451877+Apemerson@users.noreply.github.com>
2022-07-26 15:39:04 -0300

    Add files via upload

---
+++ src/cardapio.js
@@ -0,0 +1,28 @@
+const cardapio = {
+  1: {
+    id: 1,
+    descricao: "X-Burguer",
+    preco: 13.90,
+    quantidade: 0,
+  },
+  2: {
+    id: 2,
+    descricao: "x-Salada",
+    preco: 15.00,
+    quantidade: 0,
+  },
+  3: {
+    id: 3,
+    descricao: "CocaCola-2L",
+    preco: 10.99,
+    quantidade: 0,
+  },
+  4: {
+    id: 4,
+    descricao: "Pizza",
+    preco: 58.95,
+    quantidade: 0,
+  },
+};
+
+exports.menu = cardapio;
---
+++ src/informacoesCliente.js
@@ -0,0 +1,5 @@
+ const NOME_CLIENTE = "Pizzaria LaBelle";
+ const ENDERECO = "Avenida Brasil, 250, Centro - Cambé, 30000-000";
+ const TELEFONE = "(43) 9999-9999";
+
+exports.info =  {NOME_CLIENTE,ENDERECO, TELEFONE }
